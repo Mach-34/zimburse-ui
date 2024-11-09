577872da66c0a84f7b60dabec1d9eff8a780e635
Jack Gilcrest <me@jp4g.llc>
2024-11-08 18:00:07 -0700

    update import

--- src/artifacts/index.ts
+++ src/artifacts/index.ts
@@ -2,5 +2,6 @@ export {
   TokenContract,
   MultiCallEntrypointContract,
 } from "@aztec/noir-contracts.js";
-export { ZImburseEscrowContract } from "./ZImburseEscrow";
-export { ZImburseRegistryContract } from "./ZImburseRegistry";
+// export { ZImburseEscrowContract } from "./ZImburseEscrow";
+// export { ZImburseRegistryContract } from "./ZImburseRegistry";
+export { ZImburseEscrowContract, ZImburseRegistryContract } from '@mach-34/zimburse/dist/src/artifacts/contracts';
